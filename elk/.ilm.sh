7598dfe3a4609a85f6b745b3e82626522df874a3
yrimah <yrimah@student.1337.ma>
2024-03-12 18:37:48 +0000

    testing the jobs

--- elk/.ilm.sh
+++ elk/.ilm.sh
@@ -34,117 +34,119 @@ then
   curl -k -X PUT -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTICSEARCH_HOSTS}/_ilm/policy/${ILM_POLICY_NAME2}" -H 'Content-Type: application/json' -d "${ILM_POLICY_BODY}"
 
   # Associate ILM Policy with an Index Template
-  curl -k -X PUT -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTICSEARCH_HOSTS}/_template/containers_logs_template" -H 'Content-Type: application/json' -d '{
-    "index_patterns": ["containers_logs*"],
-    "settings": {
-      "index": {
-        "lifecycle": {
-          "name": "containers_logs_policy",
-          "rollover_alias": "containers_logs"
-        }
-      }
-    },
-    "mappings": {
-    "_doc": {
-      "properties": {
-        "container": {
-          "properties": {
-            "name": {
-              "type": "keyword"
-            },
-            "id": {
-              "type": "keyword"
-            }
-          }
-        },
-        "image": {
-          "properties": {
-            "name": {
-              "type": "keyword"
-            },
-            "id": {
-              "type": "keyword"
-            }
-          }
-        },
-        "status": {
-          "type": "keyword"
-        },
-        "message": {
-          "type": "text"
-        }
-      }
-    }
-  }
-  }'
+  # curl -k -X PUT -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTICSEARCH_HOSTS}/_template/containers_logs_template" -H 'Content-Type: application/json' -d '{
+  #   "index_patterns": ["containers_logs*"],
+  #   "priority": 1,
+  #   "template": {
+  #     "settings": {
+  #       "index": {
+  #         "lifecycle": {
+  #           "name": "containers_logs_policy",
+  #           "rollover_alias": "containers_logs"
+  #         }
+  #       }
+  #     },
+  #     "mappings": {
+  #       "properties": {
+  #         "container": {
+  #           "type": "nested",
+  #           "properties": {
+  #             "name": {
+  #               "type": "keyword"
+  #             },
+  #             "id": {
+  #               "type": "keyword"
+  #             }
+  #           }
+  #         },
+  #         "image": {
+  #           "type": "nested",
+  #           "properties": {
+  #             "name": {
+  #               "type": "keyword"
+  #             },
+  #             "id": {
+  #               "type": "keyword"
+  #             }
+  #           }
+  #         },
+  #         "status": {
+  #           "type": "keyword"
+  #         },
+  #         "message": {
+  #           "type": "keyword"
+  #         }
+  #       }
+  #     }
+  #   }'
 
-  curl -k -X PUT -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTICSEARCH_HOSTS}/_template/users_data_template" -H 'Content-Type: application/json' -d '{
-    "index_patterns": ["users_data*"],
-    "settings": {
-      "index": {
-        "lifecycle": {
-          "name": "users_data_policy",
-          "rollover_alias": "users_data"
-        }
-      }
-    }
-    "mappings": {
-    "_doc": {
-      "properties": {
-        "id": {
-          "type": "keyword"
-        },
-        "email": {
-          "type": "keyword"
-        },
-        "username": {
-          "type": "keyword"
-        },
-        "display_name": {
-          "type": "keyword"
-        },
-        "date_joined": {
-          "type": "date"
-        },
-        "last_login": {
-          "type": "date"
-        },
-        "avatar": {
-          "type": "keyword"
-        },
-        "friends": {
-          "type": "keyword"
-        },
-        "nb_wins": {
-          "type": "integer"
-        },
-        "nb_losses": {
-          "type": "integer"
-        },
-        "nb_plays": {
-          "type": "integer"
-        },
-        "status": {
-          "type": "keyword"
-        },
-        "is_active": {
-          "type": "boolean"
-        },
-        "is_42_user": {
-          "type": "boolean"
-        }
-      }
-    }
-  }
-  }'
+  # curl -k -X PUT -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "${ELASTICSEARCH_HOSTS}/_template/users_data_template" -H 'Content-Type: application/json' -d '{
+  #   "index_patterns": ["users_data*"],
+  #   "priority": 1,
+  #   "template": {
+  #     "settings": {
+  #       "index": {
+  #         "lifecycle": {
+  #           "name": "users_data_policy",
+  #           "rollover_alias": "users_data"
+  #         }
+  #       }
+  #     },
+  #     "mappings": {
+  #       "properties": {
+  #         "id": {
+  #           "type": "keyword"
+  #         },
+  #         "email": {
+  #           "type": "keyword"
+  #         },
+  #         "username": {
+  #           "type": "keyword"
+  #         },
+  #         "display_name": {
+  #           "type": "keyword"
+  #         },
+  #         "date_joined": {
+  #           "type": "date"
+  #         },
+  #         "last_login": {
+  #           "type": "date"
+  #         },
+  #         "avatar": {
+  #           "type": "keyword"
+  #         },
+  #         "friends": {
+  #           "type": "keyword"
+  #         },
+  #         "nb_wins": {
+  #           "type": "integer"
+  #         },
+  #         "nb_losses": {
+  #           "type": "integer"
+  #         },
+  #         "nb_plays": {
+  #           "type": "integer"
+  #         },
+  #         "status": {
+  #           "type": "keyword"
+  #         },
+  #         "is_active": {
+  #           "type": "boolean"
+  #         },
+  #         "is_42_user": {
+  #           "type": "boolean"
+  #         }
+  #       }
+  #     }
+  #   }'
 
   # while ! nc -z https://localhost 5601; do
   #   echo "Waiting for kibana to be ready..."
   #   sleep 1
   # done
 
-  curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/config/users_stats_final_dashboard.ndjson
-  curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/config/containers_logs_final_dashboard.ndjson
+  curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/config/users_stats_dashboard.ndjson
+  curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/config/containers_logs_dashboard.ndjson
   # curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/config/export.ndjson
   # curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/kibana/dashboards/import" -H 'Content-Type: application/json' -d @/usr/share/kibana/config/containers_logs_final_dashboard.ndjson
   # curl -k -X POST -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" "https://localhost:5601/api/kibana/dashboards/import" -H 'Content-Type: application/json' -d @/usr/share/kibana/config/users_stats_final_dashboard.ndjson
