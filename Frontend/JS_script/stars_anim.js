50132c65cf1b3baa580a66692137a064dd3a040a
zowaa <elfilfatimaezzahra0@gmail.com>
2024-03-21 22:27:06 +0000

    Home done \ responsive, multilang & spa

---
+++ Frontend/JS_script/stars_anim.js
@@ -0,0 +1,36 @@
+function setStars() {
+	const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+	function multipleBoxShadow(n) {
+		let shadows = [];
+		for (let i = 0; i < n; i++) {
+			const x = Math.floor(Math.random() * vw) + 'px'; // pos_x : 0 to the viewport width 
+			const y = Math.floor(Math.random() * (vh + 2000)) - 1000 + 'px'; // pos_y : -1000px to vh+1000px
+			shadows.push(`${x} ${y} #FFF`);
+		}
+		return shadows.join(', ');
+	}
+
+	// Determine stars nb
+	let starsSmall, starsMedium, starsLarge;
+	if (vw <= 500) {
+	  starsSmall = 300;
+	  starsMedium = 100;
+	  starsLarge = 50;
+	}
+	else if (vw <= 1300) {
+	  starsSmall = 500;
+	  starsMedium = 150;
+	  starsLarge = 75;
+	}
+	else {
+	  starsSmall = 1000;
+	  starsMedium = 500;
+	  starsLarge = 300;
+	}
+
+	document.getElementById('stars').style.boxShadow = multipleBoxShadow(starsSmall);
+	document.getElementById('stars2').style.boxShadow = multipleBoxShadow(starsMedium);
+	document.getElementById('stars3').style.boxShadow = multipleBoxShadow(starsLarge);
+}
--- Frontend/router.js
+++ Frontend/router.js
@@ -1,4 +1,3 @@
-const urlPageTitle = "SPA router";
 const urlRoutes = {
 	404: {
 		template: "/Frontend/Pages/404.html",
@@ -8,6 +7,8 @@ const urlRoutes = {
 		template: "/Frontend/Pages/home.html",
 		description: "This is Home page",
 	},
+
+
 
 
 	"/about": {
@@ -40,17 +41,11 @@ const urlRoute = (event) => {
 const urlLocationHandler = async () => {
 
 	const location = window.location.pathname;
-    // localStorage.setItem("userLanguage", "en");
 	if (location.length == 0) {
 		location = "/";
 	}
 
-	
-
-
 	const route = urlRoutes[location] || urlRoutes[404];
-	// const html = await fetch(route.template).then((response) => response.text());
-	
 	const response = await fetch(route.template);
 
 	let html;
@@ -61,41 +56,25 @@ const urlLocationHandler = async () => {
 		console.error("Error: " + response.status);
 	}
 
-	
 	document.getElementById("body_to_load").innerHTML = html;
-	
-
-	document
-	.querySelector('meta[name="description"]')
-	.setAttribute("content", route.description);
+	document.querySelector('meta[name="description"]').setAttribute("content", route.description);
 	
 	const init_lang = getSavedLanguagePreference();
-	console.log(init_lang);
-	//print lang stored in local storage
-console.log("mokk");
-	// console.log("Language stored in local storage: " + init_lang);
 	const language = await import(`./Lang_files/lang.${init_lang}.js`);
 	saveLanguagePreference(init_lang);
 	applyLanguageToContent(language.default);
-	if (location === '/' || location === '/Frontend/Pages/home.html') {
+
+	if (location === '/') {
         runPongAnimation();
     }
 };
 
 function getSavedLanguagePreference() {
-    // return localStorage.getItem("userLanguage") || "fr";
-	if (localStorage.getItem("userLanguage") === null) {
-		console.log("3afak hna");
-		console.log(localStorage.getItem("userLanguage"));
-		return "en";
-	}
-	return localStorage.getItem("userLanguage") ;
+	return localStorage.getItem("userLanguage") || "en";
 }
 
 
 function changeLanguage( selectedLanguage ) {
-    // const selectedLanguage = document.getElementById("language").value;
-
     loadLanguage(selectedLanguage);
 }
 
@@ -125,53 +104,13 @@ function applyLanguageToContent(lang) {
 	}
 }
 
-
-
-
-function setStars() {
-	const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-
-	function multipleBoxShadow(n) {
-	  let shadows = [];
-	  for (let i = 0; i < n; i++) {
-		const x = Math.floor(Math.random() * vw) + 'px';
-		const y = Math.floor(Math.random() * (vh + 2000)) - 1000 + 'px'; // Generate y values from -1000px to vh+1000px
-		shadows.push(`${x} ${y} #FFF`);
-	  }
-	  return shadows.join(', ');
-	}
-
-	// Determine the number of stars based on the viewport width
-	let starsSmall, starsMedium, starsLarge;
-	if (vw <= 600) {
-	  // Small devices
-	  starsSmall = 300; // Adjust as needed
-	  starsMedium = 100; // Adjust as needed
-	  starsLarge = 50; // Adjust as needed
-	} else if (vw <= 1200) {
-	  // Medium devices
-	  starsSmall = 500; // Adjust as needed
-	  starsMedium = 150; // Adjust as needed
-	  starsLarge = 75; // Adjust as needed
-	} else {
-	  // Large devices
-	  starsSmall = 1000; // Adjust as needed
-	  starsMedium = 500; // Adjust as needed
-	  starsLarge = 300; // Adjust as needed
-	}
-
-	document.getElementById('stars').style.boxShadow = multipleBoxShadow(starsSmall);
-	document.getElementById('stars2').style.boxShadow = multipleBoxShadow(starsMedium);
-	document.getElementById('stars3').style.boxShadow = multipleBoxShadow(starsLarge);
-  }
 window.onpopstate = urlLocationHandler;
-// window.onload = urlLocationHandler;
 
 window.onload = () => {
     setStars();
-    urlLocationHandler(); // Make sure to call the original onload function
+    urlLocationHandler();
 };
-window.onresize = setStars;
 
-
+window.onresize = () => {
+	setStars();
+} 
